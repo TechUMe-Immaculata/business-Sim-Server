547d811df9917f0f25ff6d0ec7f40516ed460afb
mardpaul <mard.paulo@gmail.com>
2015-02-19 13:21:53 -0500

    YESSS MAN
    Very good progress , working on creating match

--- Server/cloud/main.js
+++ Server/cloud/main.js
@@ -343,27 +343,11 @@ queryUser.find({
 });
 
 
-var GameManager = require('cloud/game_matching.js');
+Parse.Cloud.define("createtime", function(request, response) {
 
-Parse.Cloud.define("joinNewGame", function(request, response) {
-  console.log("Incoming join request from " + request.user);
-  
-  if (request.user) {
-    GameManager.joinAnonymousGame(request.user, {
-      success: function(match, isTurn) {
-        response.success(match);
-      },
-      error: function(error) {
-        console.log(error);
-        response.error("An error has occured.");
-      }
-    });
-  } else {
-    response.error("Authentication failed");
-  }
+
+
 });
-
-
 
 
 
